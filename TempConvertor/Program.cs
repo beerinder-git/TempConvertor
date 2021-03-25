8010a1f37a6e787dea7465f07ace4500531dbedf
jot123971 <kaurjyoti67871@gmail.com>
2021-03-26 10:56:30 +1300

    added code for Temperature conversion

---
+++ TempConvertor/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TempConvertor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("1. Convert to Fehrenheit");
+                Console.WriteLine("2. Convert to Celsius");
+                Console.WriteLine("Please select the Option: ");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+                double value = 0.0;
+                double temperature = 0.0;
+
+            
+                switch (option)
+                    {
+                    case 1:
+                        Console.WriteLine("Enter temperature in Celsius ");
+                        value = Convert.ToInt32(Console.ReadLine());
+                        temperature = (value * 9/5) + 32;
+                        Console.WriteLine("Temperature in Celsius: " + temperature + "F");
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter temperature in Fehrenheit ");
+                        value = Convert.ToInt32(Console.ReadLine());
+                        temperature = (value - 32 ) * 0.5;
+                        Console.WriteLine("Temperature in Celsius: " + temperature + "C");
+                        break;
+                
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
